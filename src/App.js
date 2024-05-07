9da56afabf15c7c42af27baa4ef4ccbdc3756209
raj <33422414+rajat4722@users.noreply.github.com>
2024-05-07 11:27:47 +0530

    image_gallery_created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import logo from './logo.png'; 
+import headerImage from './header.png'; 
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchImages();
+  }, [page]);
+
+  const fetchImages = async () => {
+    try {
+      const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=10`);
+      const data = await response.json();
+      setImages(prevImages => [...prevImages, ...data]);
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
+
+  const loadMoreImages = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+      <div>
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <h1 className="gallery-title">Image Gallery</h1>
+        <div>
+        {/* <img src={headerImage} alt="Header" className="header-image" /> */}
+        </div>
+       
       </header>
+      <div className="image-container">
+        {images.map(image => (
+          <div key={image.id} className="image-item">
+            <img src={image.download_url} alt={`Image ${image.id}`} />
+          </div>
+        ))}
+      </div>
+    
+<button onClick={loadMoreImages} className="load-more-btn">Load more</button>
+
     </div>
   );
 }
