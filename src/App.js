756e4ae609720ff19762d3c99cef86dc557e8cf3
raj <33422414+rajat4722@users.noreply.github.com>
2024-05-07 13:53:29 +0530

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import logo from './logo.png'; 
-import headerImage from './header.png'; 
+// import headerImage from './header.png'; 
 
 function App() {
   const [images, setImages] = useState([]);
